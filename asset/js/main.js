ae96680f3d45102121c8b75db234a8aaf51654df
danielplebani <danielpleba03@gmail.com>
2023-09-05 15:43:35 +0200

    last changes

--- asset/js/main.js
+++ asset/js/main.js
@@ -5,19 +5,19 @@ Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina
 const dataAttuale = new Date();
 console.log('la data attuale è: ' + dataAttuale);
 
-const dataConsegna = new Date(2023, 08, 06, 09, 30, 0, 0);
+const dataConsegna = new Date(2023, 08, 06, 09, 30);
 console.log('la data di consegna è: ' + dataConsegna);
 
 console.log('è iniziato il countdown!!!');
 
 let second = Math.round((dataConsegna - dataAttuale) / 1000);
+
 const clock = setInterval(function(){
     if (second == 0) {
         clearInterval(clock);
         alert('è il momento di consegnare!!!')
     } else {
-        document.querySelector('h3').innerHTML = second;
-        console.log(second);
+        document.querySelector('h3').innerHTML = 'Mancano esattamente: ' + second + ' secondi';
         second--
     }
 }, 1000)
